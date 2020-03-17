f4bff98375cf69f8e989cf3b046a59976aba6562
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-03-17 19:17:12 -0400

    added first graph to js

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let trafficCanvas = document.getElementById('traffic-chart');
+
+let trafficData = {
+    labels: [ "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31" ],
+    datasets: [
+        {
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116,119,191,.3)',
+        borderWidth: 1,
+        }
+    ]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
+
+
+// ​let​ trafficOptions = { 
+//     aspectRatio: ​2.5​, 
+//     animation: {
+//         duration: ​0 
+//     },
+//     scales: {
+//         yAxes: [{
+//             ticks: { beginAtZero:​true
+//             } 
+//         }]
+//     },
+// legend : {
+// display: ​false }
+// };
+
+
+
+// line graph
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
