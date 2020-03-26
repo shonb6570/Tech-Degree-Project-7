406fc3f0c5857198dc08bb34101a3f1cedad6870
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-03-26 16:20:35 -0400

    messaging function added; css edits

--- js/script.js
+++ js/script.js
@@ -35,6 +35,11 @@ const dropDown = document.getElementById("myDropdown");
 const bellIcon = document.getElementById("bellIcon");
 const closeBtn = document.querySelectorAll(".close-content");
 
+    //messaging selectors 
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
 
 // <--------          Drop-down messages               ---------> //
 
@@ -254,3 +259,16 @@ const mobileChart = new Chart(mobileCanvas, {
 });
 
 
+// <--------          messaging function                ---------> //
+
+send.addEventListener('click', () => {
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+});
