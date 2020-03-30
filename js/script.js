d42d9fd720b4cf0f71b2fe7690be41cb19337e53
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-03-30 16:10:40 -0400

    sticky scroll alert banner, nav and header added

--- js/script.js
+++ js/script.js
@@ -82,25 +82,21 @@ alertBanner.addEventListener('click', e => {
 });
 
 
-   // stick alert banner to top of screen on scroll
-// When the user scrolls the page, execute myFunction
-
-window.onscroll = function() {myFunction()};
-
-// Get the header
-const header = document.getElementById("header");
+// Execute stickyDiv on scroll
+window.onscroll = function() {stickyDiv()};
 
 // Get the offset position of the navbar
-const sticky = header.offsetTop;
+const sticky = alertBanner.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyDiv() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    alertBanner.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    alertBanner.classList.remove("sticky");
   }
 }
+
 
 // <--------          Line chart                       ---------> //
 
